ea59e63e8f931db99607225de7bc737522ab0837
Ajones2471 <55244485+Ajones2471@users.noreply.github.com>
2019-09-12 10:48:45 -0400

    Add files via upload

---
+++ M2LAB_Jones.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+M2T1
+Jones
+9/12/19
+"""
+
+# Convert M2T1 into python
+
+# M2LAB
+# Andrew Jones
+# 9/12/19
+
+#//Greet user by name
+#//display their age
+#//on next birthday
+
+#//Declarations
+#//Declare String name
+#//Declare Integer age
+
+
+#Ask for name
+print ("What is your name?")
+#input name
+name = input()
+
+#Ask for age
+print ("How old are you?")
+#input age
+age = int(input())
+#Set nextYear = age + 1
+nextYear = age + 1
+#Tell them how old they will be next year
+print (name, "will be", nextYear, "in 2020!")
